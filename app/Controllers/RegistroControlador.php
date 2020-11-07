be31c7d1dfd9b38a981975e33c78dec4cb4345a2
Juan <juan.gallegomesa@gmail.com>
2020-11-07 16:27:57 -0500

    SE CERA CONSULTA PARA INSERTAR DATOS

--- app/Controllers/RegistroControlador.php
+++ app/Controllers/RegistroControlador.php
@@ -1,5 +1,7 @@
 <?php namespace App\Controllers;
 
+use App\Models\Modelopersonas;
+
 class RegistroControlador extends BaseController{
     
     public function index(){
@@ -8,9 +10,43 @@ class RegistroControlador extends BaseController{
 
 	public function registrar(){
 
-	
+		//1. Recibir los datos desde la vista
 		$nombre=$this->request->getPost("nombre");
-		echo($nombre);
+		$edad=$this->request->getPost("edad");
+		$cedula=$this->request->getPost("cedula");
+		$poblacion=$this->request->getPost("poblacion");
+		$descripcion=$this->request->getPost("descripcion");
+
+		//2. Organizar los datos que llegan de las vistas
+		// en un arreglo asociativo 
+		//(las claves deben ser iguales a los campos o atributos de la tabla en BD)
+		$datosEnvio=array(
+			"nombre"=>$nombre,
+			"edad"=>$edad,
+			"cedula"=>$cedula,
+			"poblacion"=>$poblacion,
+			"descripcion"=>$descripcion
+		);
+
+		//3. Crear un objeto del MODELO para porder 
+		//realizar la transacción en BD
+		$modeloPersonas= new Modelopersonas();
+
+		//4. Utilizar el modelo para insertar los datos
+		try{
+			
+			$modeloPersonas->insert($datosEnvio);
+			echo("Registro agregado");	
+
+
+		}catch(\Exception $error){
+
+			echo($error->getMessage());
+
+		}
+
+
+
 
 	
 
